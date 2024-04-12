0b3f2ede5acf7a592c5f2bdee34460549864e7ff
Zari Bae <ahmadazzhari@gmail.com>
2024-04-12 14:01:21 +0700

    adding role column to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->text('image')->default('/uploads/pngwin.png');
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('role', ['user', 'admin'])->defaultValue('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
