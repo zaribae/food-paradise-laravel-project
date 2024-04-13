7b729ca35e031c02a2392cbfebaefcc7c685a7b2
Zari Bae <ahmadazzhari@gmail.com>
2024-04-13 22:49:52 +0700

    home page view added

---
+++ resources/views/frontend/home/index.blade.php
@@ -0,0 +1,46 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+    {{-- Slider --}}
+    @include('frontend.home.components.slider')
+
+
+    {{-- Benefits  --}}
+    @include('frontend.home.components.benefits')
+
+
+    {{-- Item Promo  --}}
+    @include('frontend.home.components.promos-offer')
+
+
+    {{-- Cart PopUp  --}}
+    @include('frontend.home.components.cart-popup')
+
+
+    {{-- Menu Item  --}}
+    @include('frontend.home.components.menu-item')
+
+
+    {{-- Menu Picture  --}}
+    @include('frontend.home.components.menu-picture')
+
+
+    {{-- Team Members  --}}
+    @include('frontend.home.components.team-members')
+
+
+    {{-- App Download  --}}
+    @include('frontend.home.components.app-download')
+
+
+    {{-- Testimonial --}}
+    @include('frontend.home.components.testimonials')
+
+
+    {{-- Counter --}}
+    @include('frontend.home.components.counter')
+
+
+    {{-- Blog --}}
+    @include('frontend.home.components.blog')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
